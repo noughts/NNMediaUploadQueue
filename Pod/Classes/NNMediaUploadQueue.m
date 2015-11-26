fc4d6cf93bae97b928871d35f6889e64e706a315
Koichi Yamamoto <noughts@gmail.com>
2015-11-27 01:04:13 +0900

    a

--- Pod/Classes/NNMediaUploadQueue.m
+++ Pod/Classes/NNMediaUploadQueue.m
@@ -11,6 +11,7 @@
 #import "UIImage+NNUtils.h"
 #import "AFNetworking.h"
 #import "PHAsset+NNUtils.h"
+#import "NBULogStub.h"
 
 @implementation NNMediaUploadQueue{
     NSOperationQueue* _process_queue;
@@ -32,6 +33,7 @@
     if( self = [super init] ){
         _process_queue = [NSOperationQueue new];
         _network_queue = [NSOperationQueue new];
+        _network_queue.maxConcurrentOperationCount = 2;
     }
     return self;
 }
@@ -45,6 +47,7 @@
 
 /// PHAssetから画像アップロードをキューに追加
 -(void)queueUploadImageFromAsset:(PHAsset*)asset targetSize:(CGSize)targetSize{
+    NBULogInfo(@"PHAsset画像アップロードをキューに追加します");
     [_process_queue addOperationWithBlock:^{
         UIImage* img = [asset imageOfSize:targetSize];
         [self queueUploadImage:img];
@@ -54,6 +57,7 @@
 
 /// 画像アップロードをキューに追加
 -(void)queueUploadImage:(UIImage*)image{
+    NBULogInfo(@"画像アップロードをキューに追加します image=%@", image);
     [_process_queue addOperationWithBlock:^{
         NSURL* url = [image saveJPEGFileToDocumentDirectoryWithoutCompressWithMetadata:nil];
         [_network_queue addOperationWithBlock:^{
@@ -65,18 +69,22 @@
 
 
 /// 画像ファイルをアップロード
--(NSString*)uploadImageFile:(NSURL*)filePath error:(NSError**)error{
+-(NSString*)uploadImageFile:(NSURL*)fileURL error:(NSError**)error{
+    NBULogInfo(@"画像のアップロードを開始します。fileURL=%@", fileURL);
+    
     NSAssert( _imageServerUrlString, @"imageServerUrlStringを設定してください" );
     
     NSError* _error;
     
     AFHTTPRequestSerializer* requestSerializer = [[AFHTTPRequestSerializer alloc] init];
     NSMutableURLRequest *request = [requestSerializer multipartFormRequestWithMethod:@"POST" URLString:_imageServerUrlString parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-        [formData appendPartWithFileURL:filePath name:@"image" fileName:@"image.jpg" mimeType:@"image/jpg" error:nil];
+        [formData appendPartWithFileURL:fileURL name:@"image" fileName:@"image.jpg" mimeType:@"image/jpg" error:nil];
     } error:nil];
     NSData *contents = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:&_error];
     NSString *responseString = [[NSString alloc] initWithData:contents encoding:NSUTF8StringEncoding];
     //	responseString = @"<html></html>";// デバッグ用
+    
+    NBULogInfo(@"画像のアップロードが完了しました。responseString=%@", responseString);
     
     return responseString;
 }
